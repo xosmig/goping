5450619713e11d046a0d675ba59acbebd7fcbf16
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2018-05-06 13:47:34 +0300

    Cleanup tests

--- flag_parser_test.go
+++ flag_parser_test.go
@@ -10,34 +10,45 @@ type AnyError struct{}
 
 func (err AnyError) Error() string { return "" }
 
-var flagtests = []struct {
-	args            []string
-	expected_result Params
-	expected_error  error
-}{
-	{[]string{}, Params{}, WrongNumberOfArguments{expected: 1, actual: 0}},
-	{[]string{"http://some_url.com", "http://another_url"}, Params{}, WrongNumberOfArguments{expected: 1, actual: 2}},
-	{[]string{"-someNonExistingFlag"}, Params{}, AnyError{}},
-	{[]string{"http://some_url.com"},
-		Params{DefaultTimeout, DefaultInterval, DefaultCount, DefaultDeadline, "http://some_url.com"}, nil},
-	{[]string{"-timeout", "5", "-interval", "4",  "-count", "3", "-deadline", "10", "http://url.com"},
-		Params{5, 4, 3, 10, "http://url.com"}, nil},
-}
-
 func TestFlagParser(t *testing.T) {
-	for _, tt := range flagtests {
-		res, err := ParseCommandLine(tt.args, ioutil.Discard)
+	var flagTests = []struct {
+		testCaseName   string
+		args           []string
+		expectedResult Params
+		expectedError  error
+	}{
+		{"Minimal_working_example",
+			[]string{"some_url.com"},
+			Params{DefaultTimeout, DefaultInterval, DefaultCount, DefaultDeadline, "some_url.com"}, nil},
+		{"No_url_provided",
+			[]string{},
+			Params{}, WrongNumberOfArguments{}},
+		{"Two_urls_provided",
+			[]string{"http://some_url.com", "another_url"},
+			Params{}, WrongNumberOfArguments{}},
+		{"Nonexistent_flag",
+			[]string{"-someNonExistentFlag", "some_url.com"},
+			Params{}, AnyError{}},
+		{"Complete_example",
+			[]string{"-timeout", "5", "-interval", "4", "-count", "3", "-deadline", "10", "url.com"},
+			Params{5, 4, 3, 10, "url.com"}, nil},
+	}
 
-		switch {
-		case tt.expected_error == (AnyError{}):
-			if err == nil {
-				t.Errorf("On input %v expected an error.", tt.args)
+	for _, tc := range flagTests {
+		t.Run(tc.testCaseName, func(t *testing.T) {
+			res, err := ParseCommandLine(tc.args, ioutil.Discard)
+
+			switch {
+			case tc.expectedError == (AnyError{}):
+				if err == nil {
+					t.Errorf("On input %v expected an error.", tc.args)
+				}
+			case reflect.TypeOf(err) != reflect.TypeOf(tc.expectedError):
+				t.Errorf("On input %v expected an error of type: %T, actual error type: %T.",
+					tc.args, tc.expectedError, err)
+			case res != tc.expectedResult:
+				t.Errorf("On input %v expected result: %v, actual result: %v", tc.args, tc.expectedResult, res)
 			}
-		case reflect.TypeOf(err) != reflect.TypeOf(tt.expected_error):
-			t.Errorf("On input %v expected an error of type: %T, actual error type: %T.",
-				tt.args, tt.expected_error, err)
-		case res != tt.expected_result:
-			t.Errorf("On input %v expected result: %v, actual result: %v", tt.args, tt.expected_result, res)
-		}
+		})
 	}
 }
